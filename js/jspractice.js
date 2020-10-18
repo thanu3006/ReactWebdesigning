517a7323b1e991f7d977d4ee749bb7ed24223c30
thanu3006 <prasadamthanuja@gmail.com>
2020-10-18 16:27:51 +0530

    for in loop

--- js/jspractice.js
+++ js/jspractice.js
@@ -1,9 +1,10 @@
 var details={
     name:"Apssdc",
-    Year:"2015",
+    Year:2015,
     courses:["webdesign using react", "python", "Android"]
 }
 
-for(var i=0; i<details.courses.length; i++) {
-    document.write(details.courses[i] + "<br>");
+]
+for(var i in details) {
+    document.write(details[i]+"<br>")
 }
