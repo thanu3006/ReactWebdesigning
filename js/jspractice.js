a1c92dc4328ea369a05c383d9259fc3d6edc5d75
thanu3006 <prasadamthanuja@gmail.com>
2020-10-18 18:19:07 +0530

    js functions

--- js/jspractice.js
+++ js/jspractice.js
@@ -4,7 +4,46 @@ var details={
     courses:["webdesign using react", "python", "Android"]
 }
 
-]
-for(var i in details) {
-    document.write(details[i]+"<br>")
+//for(var i=0; i<details.courses.length; i++) {
+//    document.write(details.courses[i] + "<br>");
+//}
+
+//for(var i in details) {
+//    document.write(details[i]+"<br>");
+//}
+
+//forof(strings,arryas , not for object)
+//var name="thanuja prasadam"
+//for(var i of name){
+//    document.write(i , "<br>");
+//}
+
+//for(var i of details.courses){
+//    document.write(i , "<br>");
+//}
+
+//var age=20;
+//if(age<70){
+//document.write("you are Alive");
+//}
+//else if(age>80){
+//document.write("Life almost completes")
+//}
+//else{
+//    document.write("Death")
+//}
+
+ //var age=20;
+ //var status = (age == 70) ? ' life Almost completed' : 'Alive';
+ //document.write(status);
+
+function addition(j,k){
+    window.alert(j+k);
 }
+addition(89,11);
+
+
+
+
+
+ 
